1421cc14177ac53b4fb3ef0c5d355cf066a8595d
Andres_Berrio <andrewsolutions81@gmail.com>
2022-07-01 11:40:02 -0500

    feature/creation Andres B answeres javascripting

---
+++ javascripting/andrés_berrio_forero/if-statements.js
@@ -0,0 +1,4 @@
+let fruit = 'orange';
+console.log(`"The fruit name has more than five
+characters." if the length of the value of fruit is greater than five.
+Otherwise, print "The fruit name has five characters or less."`)
