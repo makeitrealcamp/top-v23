e74a16e2d69a35ae72739734b7ac3ec61a553403
Andres_Berrio <andrewsolutions81@gmail.com>
2022-07-04 14:49:50 -0500

    refractor/ added notation to my-first-io.js andresb

--- learnyounode/andres_berrio_forero/my-first-io.js
+++ learnyounode/andres_berrio_forero/my-first-io.js
@@ -5,10 +5,10 @@ console (stdout), similar to running cat file | wc -l.
 The full path to the file to read will be provided as the first
 command-line argument (i.e., process.argv[2]). You do not need to make
 your own test file. */
-
+//file system require file system
 var fs = require('fs');
-
+//the contents of readfile sinc obeject since de 3rd position 
 var contents = fs.readFileSync(process.argv[2]);
-var lines = contents.toString().split('\n').length - 1;
+var lines = contents.toString().split('\n').length - 1; //lines are contents to string split using \n , the lenght -1
 
 console.log(lines);
