54f3c4d8ea14b48c826ee9412f3aaca70dd8154b
Khriztian Moreno <khriztianmoreno@gmail.com>
2022-07-22 14:47:42 -0500

    feat: update workshop fetch

--- examples/json-server/src/services/characters.js
+++ examples/json-server/src/services/characters.js
@@ -6,35 +6,17 @@ export async function getCharacters() {
 }
 
 export async function getCharacter(id) {
-  const response = await fetch(`${BASE_URL}/characters/${id}`);
-  return await response.json();
+
 }
 
 export async function createCharacter(character) {
-  const response = await fetch(`${BASE_URL}/characters`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(character),
-  });
-  return await response.json();
+
 }
 
 export async function updateCharacter(character) {
-  const response = await fetch(`${BASE_URL}/characters/${character.id}`, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(character),
-  });
-  return await response.json();
+
 }
 
 export async function deleteCharacter(id) {
-  const response = await fetch(`${BASE_URL}/characters/${id}`, {
-    method: 'DELETE',
-  });
-  return await response.json();
+
 }
