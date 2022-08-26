37082ba938054e0aa7bd524084fac1edcaa5a871
Khriztian Moreno <khriztianmoreno@gmail.com>
2022-08-26 10:17:19 -0500

    feat: add auth0 example

---
+++ examples/auth0-social-login/src/App.js
@@ -0,0 +1,32 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+import logo from './logo.svg';
+import './App.css';
+import { useEffect } from "react";
+
+function App() {
+  const { loginWithRedirect, user, isAuthenticated, logout } = useAuth0();
+
+
+  return (
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+
+      {
+        isAuthenticated ? (
+          <>
+            <h1>Welcome {user.name}</h1>
+            <div>
+              <img src={user.picture} alt={`Avatar ${user.name}`} />
+            </div>
+            <button onClick={logout}> Logout </button>
+          </>
+        ) : (
+          <button onClick={loginWithRedirect}> Login </button>
+        )
+      }
+    </div>
+  );
+}
+
+export default App;
